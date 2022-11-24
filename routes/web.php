6c1f3f92ec3e9046413c3e94becc310d18c09803
saprabotics <muhammad.mohsin@crowdbotics.com>
2022-11-24 14:05:21 +0500

    udpated the rates link

--- routes/web.php
+++ routes/web.php
@@ -25,13 +25,16 @@ Route::get('/', function () {
 
 
 
-    $json_data = ["name" => "Sapra Service", "callback_url" => env('APP_URL') . "/api/rates", "service_discovery" => true];
+    $json_data = ["carrier_service_id" => 65528168758, "name" => "Sapra Service", "callback_url" => env('APP_URL') . "/api/rates", "service_discovery" => true];
     // $json_data = json_encode($json_data);
 
     $data_to_be_saved = ['carrier_service' => $json_data];
-    // $shopApi = $shop->api()->rest('POST', '/admin/api/2022-10/carrier_services.json', $data_to_be_saved, ["Content-Type" => "application/json"]);
-    $shopApi = $shop->api()->rest('GET', '/admin/api/2022-10/carrier_services.json');
-    // $shopApi = $shop->api()->rest('GET', '/admin/api/2022-10/webhooks.json');
+    $shopApi = $shop->api()->rest('PUT', '/admin/api/2022-10/carrier_services/65528168758.json', $data_to_be_saved, ["Content-Type" => "application/json"]);
+
+    echo  json_encode($shopApi);
+
+    // $shopApi = $shop->api()->rest('GET', '/admin/api/2022-10/carrier_services.json');
+    $shopApi = $shop->api()->rest('GET', '/admin/api/2022-10/webhooks.json');
 
     echo  json_encode($shopApi);
 
